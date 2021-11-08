adbd85bd6f1d984f21aa400ce04f9f4018fcd099
flibook <93146677+flibook@users.noreply.github.com>
2021-11-08 10:59:22 +0300

    Python file

--- .idea/test.py
+++ .idea/test.py
@@ -1,37 +1,18 @@
-# Import the modules
-import sys
-import random
-
-ans = True
-
-while ans:
-    question = raw_input("Ask the magic 8 ball a question: (press enter to quit) ")
-    
-    answers = random.randint(1,8)
-    
-    if question == "":
-        sys.exit()
-    
-    elif answers == 1:
-        print "It is certain"
-    
-    elif answers == 2:
-        print "Outlook good"
-    
-    elif answers == 3:
-        print "You may rely on it"
-    
-    elif answers == 4:
-        print "Ask again later"
-    
-    elif answers == 5:
-        print "Concentrate and ask again"
-    
-    elif answers == 6:
-        print "Reply hazy, try again"
-    
-    elif answers == 7:
-        print "My reply is no"
-    
-    elif answers == 8:
-        print "My sources say no"
+def is_port_open(host, port):
+    """
+    determine whether `host` has the `port` open
+    """
+    # создает новый сокет
+    s = socket.socket()
+    try:
+        # пытается подключиться к хосту через этот порт
+        s.connect((host, port))
+        # сделайте таймаут, если хотите немного быстрее (с меньшей точностью)
+        # s.settimeout(0.2)
+    except:
+        # не могу подключиться, порт закрыт
+        # return false
+        return False
+    else:
+        # соединение установлено, порт открыт!
+        return True
